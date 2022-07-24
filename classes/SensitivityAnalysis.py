f943dec3cda0bf30e904ff3a3b230474bd5c4c14
Vlad Matei <vlad.mateei@gmail.com>
2022-07-24 12:47:49 +0100

    Analysis of results from overnight simulations

--- classes/SensitivityAnalysis.py
+++ classes/SensitivityAnalysis.py
@@ -2,6 +2,8 @@ import json
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from IPython.core.display import display
 from mpl_toolkits import mplot3d
 
 def calculate_rmse(simulation_price_history, gme_price_history):
@@ -50,9 +52,46 @@ def write_results_dict_to_file(results_dict, file_name):
 def load_results(file_name):
     f = open(file_name)
     data = json.load(f)
-    return data
+    df = pd.DataFrame.from_dict(data, orient='index')
+    return df
 
+
+def analyse_results(sa_df):
+    rmse_values = sa_df['rmse']
+    max_prices = sa_df["max_price"]
+    min_prices = sa_df["min_price"]
+    extract_statistics(rmse_values, "rmse")
+    extract_statistics(max_prices, "max prices")
+    extract_statistics(min_prices, "min prices")
+    max_rmse_idx = sa_df["rmse"].idxmax()
+    min_rmse_idx = sa_df["rmse"].idxmin()
+
+    min_price_idx = sa_df["min_price"].idxmin()
+    max_price_idx = sa_df["max_price"].idxmax()
+
+    display(sa_df.loc[max_rmse_idx])
+    print()
+    display(sa_df.loc[min_rmse_idx])
+    print()
+    display(sa_df.loc[max_price_idx])
+    print()
+    display(sa_df.loc[min_price_idx])
+
+def extract_statistics(list_, title):
+    mean = np.mean(list_)
+    std_dev = np.std(list_)
+    max = np.max(list_)
+    min = np.min(list_)
+    q3, q1 = np.percentile(list_, [75, 25])
+    iqr = q3 - q1
+    print(f"Mean of {title} values is:  {mean}")
+    print(f"Max of {title} values is:  {max}")
+    print(f"Min of {title} values is:  {min}")
+    print(f"Std dev of {title} values is:  {std_dev}")
+    print(f"Interquantile range of {title} is: {iqr}")
+
+    print()
 
 if __name__ == '__main__':
-    price_dict = load_results("sensitivity_analysis_price_dict")
-    stop = 0
+    sa_df = load_results("ofat_sa_results")
+    analyse_results(sa_df)
