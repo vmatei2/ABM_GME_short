e09f6b6176c92481d779cf4fe9ae8c15f0ec4377
Vlad Matei <vlad.mateei@gmail.com>
2022-08-04 11:26:13 +0100

    Implemented gamma variation example to support delta hedging analysis

--- helpers/calculations_helpers.py
+++ helpers/calculations_helpers.py
@@ -1,5 +1,9 @@
+import math
 from datetime import datetime
-
+import seaborn as sns
+sns.set_style("darkgrid")
+import matplotlib.pyplot as plt
+import scipy.stats as st
 import numpy as np
 
 
@@ -50,5 +54,47 @@ def check_and_convert_imaginary_number(number):
     return number
 
 
+def calculate_d1(S, K, r, volatility, T, t=0):
+    d1 = (np.log(S / K) + (r + (volatility ** 2) / 2) * T) / (T - t)
+    return d1
+
+
+def calculate_pdf(d1):
+    pdf = (math.e ** (-math.pow(d1, 2) / 2)) / (np.sqrt(2 * np.pi))
+    return pdf
+
+
+def calculate_gamma(S, K, r, volatility, T):
+    d1 = calculate_d1(S, K, r, volatility, T)
+    pdf = calculate_pdf(d1)
+    gamma = pdf / (S * volatility * np.sqrt(T))
+    return gamma
+
+
+def gamma_variation(K, r, volatility, T):
+    all_gammas = []
+    stock_prices = []
+    for stock_price in range(0, int(2.5 * K)):
+        gamma = calculate_gamma(stock_price, K, r, volatility, T=T)
+        all_gammas.append(gamma)
+        stock_prices.append(stock_price)
+    return all_gammas, stock_prices
+
+
+def plot_gamma_variation(all_gammas, stock_prices):
+    plt.figure(figsize=(10, 10))
+    plt.plot(stock_prices, all_gammas)
+    plt.xlabel("Stock Price", fontsize=18)
+    plt.ylabel("Gamma ($\Gamma$)", fontsize=18)
+    plt.xticks([i for i in stock_prices if i % 10 == 0], fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.title("Variation of $\Gamma$ with stock price for an option with $K=60$", fontsize=20)
+    plt.show()
+
+
 if __name__ == '__main__':
-    print_current_time()
+    all_gammas, stock_prices = gamma_variation(K=60, r=0.05, volatility=0.2, T=0.5)
+    plot_gamma_variation(all_gammas, stock_prices)
+
+    test_gamma = calculate_gamma(S=49, K=50, r=0.05, volatility=0.2, T=0.3846)
+    print(test_gamma)
